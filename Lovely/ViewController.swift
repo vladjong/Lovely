53a231f4f1fd595fc2dd441e6afea3028a5bbf99
vaGaidenko <va.gaidenko@gmail.com>
2022-05-23 17:48:55 +0700

    Initial Commit

---
+++ Lovely/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lovely
+//
+//  Created by Владислав Гайденко on 23.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
